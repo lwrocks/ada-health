c2fb9f62af89cd6d140761ed6006d8166ecc55d0
Laurel Wright <wright.laureld@gmail.com>
2021-05-27 14:49:05 +0200

    Starting CSS

---
+++ ada-challenge/src/ToDoForm.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+const ToDoForm = ({addTask}) => {
+    const [userInput, setUserInput] = useState('');
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTask(userInput);
+        setUserInput('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={userInput} type='text' onChange={handleChange} placeholder='What do you need to do?'></input>
+            <button className='todo-submit'>Submit</button>
+        </form>
+    )
+}
+
+export default ToDoForm; 
